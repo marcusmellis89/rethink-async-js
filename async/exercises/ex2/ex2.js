6387b3844e81b88b3be4dd9d751a6ae17f34c315
marcusmellis89 <m.m.ellis89@gmail.com>
2016-09-19 10:35:10 -0700

    [ex2]async thunk attempt

--- async/exercises/ex2/ex2.js
+++ async/exercises/ex2/ex2.js
@@ -19,9 +19,42 @@ function output(text) {
 
 // **************************************
 
-function getFile(file) {
-	// what do we do here?
-}
+// function getFile(file) {
+//
+// }
 
 // request all files at once in "parallel"
 // ???
+
+//make the thunk
+var makeThunk = function(fn){
+	var args = [].slice.call(arguments,1);
+
+	return function(cb){
+		args.push(cb);
+		fn.apply(null,args);
+	};
+};
+
+//creating async thunks for each ajax call
+var getFileOne = makeThunk(fakeAjax, 'file1');
+var getFileTwo = makeThunk(fakeAjax, 'file2');
+var getFileThree = makeThunk(fakeAjax, 'file3');
+
+
+//requesting all files in order
+var request = function(){
+
+	getFileOne(function(response){
+		output(response);
+		getFileTwo(function(data){
+			output(data);
+			getFileThree(function(reply){
+				output(reply);
+			});
+		});
+	});
+
+}
+
+request();
