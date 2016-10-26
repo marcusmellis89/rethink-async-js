4ca1f9ee592ad7acf6e0d6ad962013498eb0954c
marcusmellis89 <m.m.ellis89@gmail.com>
2016-10-26 11:32:26 -0700

    [observable]refreshing specific user

--- observables/RxIntro/ansync_req_res.js
+++ observables/RxIntro/ansync_req_res.js
@@ -1,6 +1,17 @@
 let refreshButton = document.querySelector(".refresh");
+let closeButton1 = document.querySelector(".close1");
+let closeButton2 = document.querySelector(".close2");
+let closeButton3 = document.querySelector(".close3");
+
+
 
 let refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+let close1Clicks = Rx.Observable.fromEvent(closeButton1, 'click');
+let close2Clicks = Rx.Observable.fromEvent(closeButton2, 'click');
+let close3Clicks = Rx.Observable.fromEvent(closeButton3, 'click');
+
+
 
 let startUpRequestStream = Rx.Observable.just("http://api.github.com/users");
 
@@ -17,27 +28,31 @@ let responseStream =
   //merging with stream that does initial request
   .merge(startUpRequestStream)
   //using flatMap inorder to flatten metastream
-  .flatMap((requestURL) => Rx.Observable.fromPromise(fetch(requestURL)););
+  .flatMap((requestURL) => Rx.Observable.fromPromise(fetch(requestURL));)
+  .shareReplay(1);
 
 responseStream.subscribe( (response) => {
   console.log(response);
 })
 
 
-//creates a mapped response stream that emits one random user
-let createSuggestionStream = (stream) => {
-   return stream
-   .map(userList =>
-    userList[Math.floor(Math.random()*userList.length)]
-  )
-  .startWith(null)
-  .merge(refreshClickStream.map(evt => null));
-  ;
+const getRandomUser = (list) => {
+  list[Math.floor(Math.random()*list.length)]
 };
 
-let suggestion1Stream = createSuggestionStream(responseStream);
-let suggestion2Stream = createSuggestionStream(responseStream);
-let suggestion3Stream = createSuggestionStream(responseStream);
+//creates a mapped response stream that emits one random user
+let createSuggestionStream = (stream, closeStream) => {
+
+  return stream
+   .map(getRandomUser)
+   .startWith(null)
+   .merge(refreshClickStream.map(evt => null))
+   .merge(closeStream.withLatestFrom(stream, (closeEvt,userList) => getRandomUser(userList)));
+};
+
+let suggestion1Stream = createSuggestionStream(responseStream, close1Clicks);
+let suggestion2Stream = createSuggestionStream(responseStream, close2Clicks);
+let suggestion3Stream = createSuggestionStream(responseStream, close3Clicks);
 
 //render 'emitted' user to the DOM
 const renderSuggestion = (userdata, selector) => {
