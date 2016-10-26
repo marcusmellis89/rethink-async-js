9b29d7d9dc44e196d45063a6f9c532700b4f8a3d
marcusmellis89 <m.m.ellis89@gmail.com>
2016-10-26 08:22:24 -0700

    [observable] rendering to DOM

--- observables/RxIntro/ansync_req_res.js
+++ observables/RxIntro/ansync_req_res.js
@@ -9,3 +9,40 @@ let responseStream = requestStream
 responseStream.subscribe( (response) => {
   console.log(response);
 })
+
+
+//creates a mapped response stream that emits one random user
+let createSuggestionStream = (stream) => {
+   return stream.map(userList =>
+    userList[Math.floor(Math.random()*userList.length)]
+  );
+};
+
+let suggestion1Stream = createSuggestionStream(responseStream);
+let suggestion2Stream = createSuggestionStream(responseStream);
+let suggestion3Stream = createSuggestionStream(responseStream);
+
+//render 'emitted' user to the DOM
+const renderSuggestion = (userdata, selector) => {
+  const element = document.getElementById(selector);
+  const usernameEl = element.querySelector('.username');
+
+  usernameEl.href = userdata.html_url;
+  usernameEl.textContent = userdata.login;
+
+  const imageEl = element.querySelector('img');
+  imageEl.src = userdata.avatar_url;
+
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+})
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+})
